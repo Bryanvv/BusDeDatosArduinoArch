c20466238b6ca539421d6af72bd6f5d11d6465d6
Bryan Velasquez <bryanvelasquez@Bryans-MacBook-Pro.local>
2018-03-17 10:11:05 -0600

    commit

---
+++ busdedatos_nodejs/index.js
@@ -0,0 +1,40 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Serial = require('serialport');
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '318729897:AAHVwmMXACXet8pM90PuK7P96NUXw4bEU80';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+port.write(resp);
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+
+
+var OpcionSerial = {
+baudRate: 9600,
+autoOpen: true
+
+}
+var Puerto = "/COMXX";
+
+var port = new SerialPort(Puerto, OpcionSerial);
